04da6152d13debc1ce5f108dc4e308157529b845
Diska Romandoni <diskarmn@gmail.com>
2023-10-12 09:58:27 +0700

    commitnya

--- app.py
+++ app.py
@@ -1,17 +1,17 @@
-import os
-from os.path import join, dirname
+# import os
+# from os.path import join, dirname
 from dotenv import load_dotenv
 from flask import Flask, render_template
-import pymongo 
+# import pymongo 
 
-dotenv_path = join(dirname(__file__), '.env')
-load_dotenv(dotenv_path)
+# dotenv_path = join(dirname(__file__), '.env')
+# load_dotenv(dotenv_path)
 
-MONGODB_URI = os.environ.get("MONGODB_URI")
-DB_NAME =  os.environ.get("DB_NAME")
+# MONGODB_URI = os.environ.get("MONGODB_URI")
+# DB_NAME =  os.environ.get("DB_NAME")
 
-client = pymongo.MongoClient(MONGODB_URI)
-db=client[DB_NAME]
+# client = pymongo.MongoClient(MONGODB_URI)
+# db=client[DB_NAME]
 
 app = Flask(__name__)
 
